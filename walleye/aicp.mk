d6608be3cf37e9f0f401e6242494fc4a58f48c07
Zipsnet <hcolmenares@gmail.com>
2018-04-04 14:23:11 -0400

    muskie: AICPfy
    Change-Id: I9f466fd0de5ae20004d8e387cd5116ae4900bdf1

--- walleye/aicp.mk
+++ walleye/aicp.mk
@@ -11,7 +11,7 @@ $(call inherit-product, device/google/muskie/aosp_walleye.mk)
 -include device/google/wahoo/device-lineage.mk
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_walleye
+PRODUCT_NAME := aicp_walleye
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 2
 TARGET_MANUFACTURER := Google
